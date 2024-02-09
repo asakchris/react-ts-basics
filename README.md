d59aed6c747f5b080c1f8790fb3842e965395c3e
Kamaraj Christopher <k.appadurai@spglobal.com>
2024-02-09 14:36:09 -0600

    first commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# React TypeScript Basics
+
+This project is created by running `npm create vite@latest react-ts-basics` command
+
+### Local environment setup
+
+1. Use any editor/command line tool and run `npm install` from root of the project directory to download all dependencies
+1. Start the applications in development mode by running `npm run dev` command
+1. Access application using [this](http://localhost:5173/) link
---
+++ src/components/CourseGoal.tsx
@@ -0,0 +1,45 @@
+import { type PropsWithChildren } from 'react';
+
+// import { type FC, type ReactNode } from 'react';
+
+// interface CourseGoalProps {
+//   title: string;
+//   children: ReactNode;
+// }
+
+type CourseGoalProps = PropsWithChildren<{
+  id: number;
+  title: string;
+  onDelete: (id: number) => void;
+}>;
+
+export default function CourseGoal({
+  id,
+  title,
+  children,
+  onDelete,
+}: CourseGoalProps) {
+  return (
+    <article>
+      <div>
+        <h2>{title}</h2>
+        {children}
+      </div>
+      <button onClick={() => onDelete(id)}>Delete</button>
+    </article>
+  );
+}
+
+// const CourseGoal: FC<CourseGoalProps> = ({ title, children }) => {
+//   return (
+//     <article>
+//       <div>
+//         <h2>{title}</h2>
+//         {children}
+//       </div>
+//       <button>Delete</button>
+//     </article>
+//   );
+// };
+
+// export default CourseGoal;
